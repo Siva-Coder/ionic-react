a4a022380f4b47fbc9e4a7a72f06c0800575368e
siva-coder <sambasivarao1253@gmail.com>
2020-09-22 19:34:12 +0530

    Updates

---
+++ precache-manifest.83b36fcca41a36155c5fbab97a8b348a.js
@@ -0,0 +1,266 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "514ba2a672e107dee1b7688236f56d86",
+    "url": "/ionic-react/index.html"
+  },
+  {
+    "revision": "ab4b5564cdcecf874225",
+    "url": "/ionic-react/static/css/4.8921c4c4.chunk.css"
+  },
+  {
+    "revision": "19932af92b08247d1dde",
+    "url": "/ionic-react/static/css/main.09c9cc82.chunk.css"
+  },
+  {
+    "revision": "0687b044b73b5cf6ecef",
+    "url": "/ionic-react/static/js/0.5ba5d006.chunk.js"
+  },
+  {
+    "revision": "d7c5b4ffe96933aaae00",
+    "url": "/ionic-react/static/js/10.26682f30.chunk.js"
+  },
+  {
+    "revision": "1d4fca2c5ce2be8bd4a9",
+    "url": "/ionic-react/static/js/11.b5169917.chunk.js"
+  },
+  {
+    "revision": "2df830509e30317ff280",
+    "url": "/ionic-react/static/js/12.c244b3dc.chunk.js"
+  },
+  {
+    "revision": "6dab7db30ed59aec0357",
+    "url": "/ionic-react/static/js/13.a3840558.chunk.js"
+  },
+  {
+    "revision": "fda198e5b0515dc678ee",
+    "url": "/ionic-react/static/js/14.d63d57e2.chunk.js"
+  },
+  {
+    "revision": "90b6b156a5f62810eddc9189ae0e43d4",
+    "url": "/ionic-react/static/js/14.d63d57e2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c0523f7ab1309b0938fa",
+    "url": "/ionic-react/static/js/15.d5fee785.chunk.js"
+  },
+  {
+    "revision": "d4ca7f006fb7d70906d8",
+    "url": "/ionic-react/static/js/16.c2bdb626.chunk.js"
+  },
+  {
+    "revision": "8936db96ca7f70e7520a",
+    "url": "/ionic-react/static/js/17.05300a51.chunk.js"
+  },
+  {
+    "revision": "ca5cabbca8af3ff48691",
+    "url": "/ionic-react/static/js/18.f988266b.chunk.js"
+  },
+  {
+    "revision": "d92fc3d50a71178c95c9",
+    "url": "/ionic-react/static/js/19.83b6ba79.chunk.js"
+  },
+  {
+    "revision": "c2e01a7fcb758c7accd0",
+    "url": "/ionic-react/static/js/20.85098702.chunk.js"
+  },
+  {
+    "revision": "75516bb8df6d78f51ab6",
+    "url": "/ionic-react/static/js/21.0bdec082.chunk.js"
+  },
+  {
+    "revision": "7bbf01632e3bf9b6915c",
+    "url": "/ionic-react/static/js/22.11e833ab.chunk.js"
+  },
+  {
+    "revision": "5e8f02e3454ee7a2384c",
+    "url": "/ionic-react/static/js/23.2e8e8231.chunk.js"
+  },
+  {
+    "revision": "0422b945317169e62256",
+    "url": "/ionic-react/static/js/24.f3bdab3c.chunk.js"
+  },
+  {
+    "revision": "0dd06179f575d87e8920",
+    "url": "/ionic-react/static/js/25.94933305.chunk.js"
+  },
+  {
+    "revision": "971d7c1575406b09e776",
+    "url": "/ionic-react/static/js/26.30f8e0f0.chunk.js"
+  },
+  {
+    "revision": "ac928e1eda768467fcaa",
+    "url": "/ionic-react/static/js/27.0c25c582.chunk.js"
+  },
+  {
+    "revision": "b009a687d68e8320d793",
+    "url": "/ionic-react/static/js/28.2bf168b7.chunk.js"
+  },
+  {
+    "revision": "2a7d0785a124d5151751",
+    "url": "/ionic-react/static/js/29.6603e602.chunk.js"
+  },
+  {
+    "revision": "e7e189659023b3ac9410",
+    "url": "/ionic-react/static/js/30.5412ac67.chunk.js"
+  },
+  {
+    "revision": "f8dd94be4cc178c01f52",
+    "url": "/ionic-react/static/js/31.fe47a611.chunk.js"
+  },
+  {
+    "revision": "df015704bba60de00e9a",
+    "url": "/ionic-react/static/js/32.6cd65845.chunk.js"
+  },
+  {
+    "revision": "94c3e30903756f52d35c",
+    "url": "/ionic-react/static/js/33.26fcc776.chunk.js"
+  },
+  {
+    "revision": "7c3ee9dae99253634483",
+    "url": "/ionic-react/static/js/34.e3423a7e.chunk.js"
+  },
+  {
+    "revision": "3404f0bfcdecfe81240f",
+    "url": "/ionic-react/static/js/35.05fec779.chunk.js"
+  },
+  {
+    "revision": "b3b54957331e26dd30dd",
+    "url": "/ionic-react/static/js/36.70a18385.chunk.js"
+  },
+  {
+    "revision": "da5fbda25b298c9fbf92",
+    "url": "/ionic-react/static/js/37.77974f2d.chunk.js"
+  },
+  {
+    "revision": "21f284d9213775d86251",
+    "url": "/ionic-react/static/js/38.0677d3e8.chunk.js"
+  },
+  {
+    "revision": "43ef49923840671dfd5d",
+    "url": "/ionic-react/static/js/39.22ba02bd.chunk.js"
+  },
+  {
+    "revision": "ab4b5564cdcecf874225",
+    "url": "/ionic-react/static/js/4.bf8aa3ba.chunk.js"
+  },
+  {
+    "revision": "c87550333da8df8ab4c80bd1d6e9defb",
+    "url": "/ionic-react/static/js/4.bf8aa3ba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c5f2e0ff996e5b49318a",
+    "url": "/ionic-react/static/js/40.929dcd34.chunk.js"
+  },
+  {
+    "revision": "aa22d9e2821c669078dd",
+    "url": "/ionic-react/static/js/41.4a103bab.chunk.js"
+  },
+  {
+    "revision": "0e5806bf617de6a43e5b",
+    "url": "/ionic-react/static/js/42.6a5cc4a3.chunk.js"
+  },
+  {
+    "revision": "a939dfd8f703dd27e29e",
+    "url": "/ionic-react/static/js/43.f03dc3c6.chunk.js"
+  },
+  {
+    "revision": "7138fee13a3a59a0ed7b",
+    "url": "/ionic-react/static/js/44.cc9b7a9a.chunk.js"
+  },
+  {
+    "revision": "b96af2e90ce7abb06480",
+    "url": "/ionic-react/static/js/45.ddf83df2.chunk.js"
+  },
+  {
+    "revision": "e78abebad494e3a00fb2",
+    "url": "/ionic-react/static/js/46.8e9ce55b.chunk.js"
+  },
+  {
+    "revision": "7a9c10d5a9daaa1f21a8",
+    "url": "/ionic-react/static/js/47.d1171f4b.chunk.js"
+  },
+  {
+    "revision": "861f210d2d93704ef1e4",
+    "url": "/ionic-react/static/js/48.da9e12ab.chunk.js"
+  },
+  {
+    "revision": "61fe93872b8352d187a4",
+    "url": "/ionic-react/static/js/49.e5fb12b2.chunk.js"
+  },
+  {
+    "revision": "2c3753dadacafad0714a",
+    "url": "/ionic-react/static/js/5.fd158aed.chunk.js"
+  },
+  {
+    "revision": "7a03562d2515509875e4",
+    "url": "/ionic-react/static/js/50.fb1faeb8.chunk.js"
+  },
+  {
+    "revision": "794b3536edab8473e080",
+    "url": "/ionic-react/static/js/51.203867f0.chunk.js"
+  },
+  {
+    "revision": "5847054128f1c1c95a9f",
+    "url": "/ionic-react/static/js/52.88ab9da5.chunk.js"
+  },
+  {
+    "revision": "383899e60ed69a9a53414c6836d91ab5",
+    "url": "/ionic-react/static/js/52.88ab9da5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "365a35afdf87a22108be",
+    "url": "/ionic-react/static/js/53.ed11c077.chunk.js"
+  },
+  {
+    "revision": "c7f637bb675e7887366e",
+    "url": "/ionic-react/static/js/54.8e839c8f.chunk.js"
+  },
+  {
+    "revision": "5e0b6ccb2a016dc8309c",
+    "url": "/ionic-react/static/js/55.cd3aa133.chunk.js"
+  },
+  {
+    "revision": "ff85172da6c0d5e50827",
+    "url": "/ionic-react/static/js/56.751d6b4f.chunk.js"
+  },
+  {
+    "revision": "576420ddd7d5f82b4331",
+    "url": "/ionic-react/static/js/57.ba14c4dd.chunk.js"
+  },
+  {
+    "revision": "e6ea7b692a53f5467055",
+    "url": "/ionic-react/static/js/58.2d32139a.chunk.js"
+  },
+  {
+    "revision": "383899e60ed69a9a53414c6836d91ab5",
+    "url": "/ionic-react/static/js/58.2d32139a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "39ced01dea39f4c4f413",
+    "url": "/ionic-react/static/js/6.92bcdac1.chunk.js"
+  },
+  {
+    "revision": "335347088e997dac70e5",
+    "url": "/ionic-react/static/js/7.00fdffa7.chunk.js"
+  },
+  {
+    "revision": "9c4b8a74f87bbb19f3ba",
+    "url": "/ionic-react/static/js/8.979e162f.chunk.js"
+  },
+  {
+    "revision": "31db43c0c18591b28405",
+    "url": "/ionic-react/static/js/9.137b91bd.chunk.js"
+  },
+  {
+    "revision": "19932af92b08247d1dde",
+    "url": "/ionic-react/static/js/main.dcfd7e45.chunk.js"
+  },
+  {
+    "revision": "633408cea77bdbc01e24",
+    "url": "/ionic-react/static/js/polyfills-css-shim.79e4451a.chunk.js"
+  },
+  {
+    "revision": "66caafeb68e0accbbe47",
+    "url": "/ionic-react/static/js/runtime-main.d280ab3c.js"
+  }
+]);
